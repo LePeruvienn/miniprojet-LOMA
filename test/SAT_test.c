5d8001d3236e10e0c2e4a385afae38a20bb73c10
Miasmax <2016maxe@gmail.com>
2026-05-08 19:07:24 +0200

    add problem cnf

---
+++ test/SAT_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    //system("/home/maxence/sat/glucose/simp/glucose ../input/problem.cnf > ../out/result.txt");
+    system("pwd");
+    return 0;
+}
